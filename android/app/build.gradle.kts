840c640eef051db6a3d1a8b11c0586a48c3d8d50
sadicGoblin <felipe.gallardo.estay@gmail.com>
2026-04-21 15:28:26 -0400

    andoird manifest genreal name changed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.hydroscan.hydroscan_cam"
+    namespace = "com.metscan.metscan_app"
     // Plugins (mobile_scanner, camera_android_camerax, etc.) require compileSdk 36 + NDK 27.
     compileSdk = 36
     ndkVersion = "27.0.12077973"
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.hydroscan.hydroscan_cam"
+        applicationId = "com.metscan.metscan_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24 // flutter_secure_storage; camera/camerax stack also supports 24+.
--- android/app/src/main/kotlin/com/metscan/metscan_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/metscan/metscan_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hydroscan.hydroscan_cam
+package com.metscan.metscan_app
 
 import io.flutter.embedding.android.FlutterActivity
 
